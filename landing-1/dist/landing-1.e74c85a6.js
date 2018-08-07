1fe0697931c9912d7c8326080ea906f02fc7cdeb
OstapHetman <prickly4528@gmail.com>
2018-08-07 15:05:40 +0300

    add layout for about section

--- landing-1/dist/landing-1.e74c85a6.js
+++ landing-1/dist/landing-1.e74c85a6.js
@@ -179,6 +179,11 @@ module.hot.accept(reloadCSS);
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
+},{"_css_loader":"..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\css-loader.js"}],"scss\\layout\\_about.scss":[function(require,module,exports) {
+
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
 },{"_css_loader":"..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
@@ -187,7 +192,9 @@ require("./scss/layout/_navigation.scss");
 require("./scss/layout/_header.scss");
 
 require("./scss/layout/_features.scss");
-},{"./scss/layout/_navigation.scss":"scss\\layout\\_navigation.scss","./scss/layout/_header.scss":"scss\\layout\\_header.scss","./scss/layout/_features.scss":"scss\\layout\\_features.scss"}],"..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+
+require("./scss/layout/_about.scss");
+},{"./scss/layout/_navigation.scss":"scss\\layout\\_navigation.scss","./scss/layout/_header.scss":"scss\\layout\\_header.scss","./scss/layout/_features.scss":"scss\\layout\\_features.scss","./scss/layout/_about.scss":"scss\\layout\\_about.scss"}],"..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
--- landing-1/index.js
+++ landing-1/index.js
@@ -2,3 +2,4 @@
 import "./scss/layout/_navigation.scss";
 import "./scss/layout/_header.scss";
 import "./scss/layout/_features.scss";
+import "./scss/layout/_about.scss";
